9afc272a3e81beeef37c11df093767217a410d3e
Julien <julienhalde9@gmail.com>
2024-11-13 22:26:25 -0500

    Added Starter Map

--- config/tiles.tsx
+++ config/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="mario-0-tiles" tilewidth="16" tileheight="16" tilecount="15" columns="5">
- <image source="../assets/images/tiles.png" width="80" height="48"/>
+<tileset version="1.10" tiledversion="1.11.0" name="pixel-put-tiles" tilewidth="16" tileheight="16" spacing="1" tilecount="180" columns="16">
+ <image source="../assets/images/tiles.png" width="271" height="169"/>
 </tileset>
